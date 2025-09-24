c2ef50f67edc895bb41f475b5296170257115ae1
matheus <matheus.valerio6@etec.sp.gov.br>
2025-09-24 14:37:54 -0300

    adição de imagens

--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -3,12 +3,13 @@ import express from 'express';
 const router = express.Router();
 
 const teamMembers = [
-  { name: "Matheus Soares Valério", photo: "membro1.jpg" },
-  { name: "Sarah Olivatto de Oliveira", photo: "membro2.jpg" },
+  { name: "Matheus Soares Valério", photo: "Matheus.jpg" },
+  { name: "Sarah Olivatto de Oliveira", photo: "Sarah.jpg" },
 ];
 
-router.get('/team', (req, res) => {
-  console.log("✔️ Rota /team acessada"); // TESTE!
+// Defina a rota como '/team'
+router.get('/', (req, res) => {
+  console.log("✔️ Rota /team acessada");
   res.render('show', { teamMembers });
 });
 
